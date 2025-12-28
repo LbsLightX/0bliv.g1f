9a8dcafdb9ab976769c2c7af18aaee2ec9989ed6
LbsLightX <shivamkumarking66@gmail.com>
2025-12-28 10:42:05 +0530

    🐛 | fix Next.js config for ESM (use .cjs)

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'export',
   images: {
     unoptimized: true,
   },
 };
-
-module.exports = nextConfig;
