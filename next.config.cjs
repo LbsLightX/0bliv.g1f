9505a276a8d1263ec4f8fcc817dc603d0e96227e
LbsLightX <shivamkumarking66@gmail.com>
2025-12-28 11:00:53 +0530

    🐛 | fix routing for GitHub Pages base path

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
+
+  basePath: '/0bliv.g1f',
+  assetPrefix: '/0bliv.g1f/',
+
   images: {
     unoptimized: true,
   },
