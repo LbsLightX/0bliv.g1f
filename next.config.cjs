19102e3218878e149e5e4c401fca7c51bf0705c8
LbsLightX <shivamkumarking66@gmail.com>
2025-12-28 12:49:09 +0530

    💀 |  archive: clear project files, pause development

--- next.config.cjs
+++
@@ -1,8 +0,0 @@
-const nextConfig = {
-  output: 'export',
-  basePath: '/0bliv.g1f',
-  assetPrefix: '/0bliv.g1f/',
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
--- src/config/basePath.ts
+++
@@ -1,7 +0,0 @@
-const BASE_PATH =
-  typeof window !== "undefined" &&
-  window.location.pathname.startsWith("/0bliv.g1f")
-    ? "/0bliv.g1f"
-    : "";
-
-export default BASE_PATH;
